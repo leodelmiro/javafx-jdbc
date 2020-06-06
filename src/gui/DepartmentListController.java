54f6f6ad4414a8ed582e303d17d2a8937d4f6e75
Leonardo Delmiro <leodelmiroms@gmail.com>
2020-06-06 11:30:37 -0300

    DepartmentService

--- src/gui/DepartmentListController.java
+++ src/gui/DepartmentListController.java
@@ -1,6 +1,9 @@
 package gui;
 
 import application.Main;
+import javafx.beans.Observable;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
@@ -9,11 +12,14 @@ import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 import model.entities.Department;
+import model.services.DepartmentService;
 
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
 
 public class DepartmentListController implements Initializable {
+    private DepartmentService service;
 
     @FXML
     private TableView<Department> tableViewDepartment;
@@ -27,9 +33,15 @@ public class DepartmentListController implements Initializable {
     @FXML
     private Button btNew;
 
+    private ObservableList<Department> observableList;
+
     @FXML
     public void onBtNewAction(){
         System.out.println("OnBtNewAction");
+    }
+
+    public void setDepartmentService (DepartmentService service){
+        this.service = service;
     }
 
     @Override
@@ -43,5 +55,14 @@ public class DepartmentListController implements Initializable {
 
         Stage stage = (Stage) Main.getMainScene().getWindow();
         tableViewDepartment.prefHeightProperty().bind(stage.heightProperty());
+    }
+
+    public void updateListView(){
+        if (service == null){
+            throw new IllegalStateException("Service was null");
+        }
+        List<Department> list = service.findAll();
+        observableList = FXCollections.observableArrayList(list);
+        tableViewDepartment.setItems(observableList);
     }
 }
---
+++ src/model/services/DepartmentService.java
@@ -0,0 +1,19 @@
+package model.services;
+
+import model.entities.Department;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DepartmentService {
+
+    public List<Department> findAll(){
+        List<Department> list = new ArrayList<>();
+
+        //MOCK
+        list.add(new Department(1, "Books"));
+        list.add(new Department(2, "Computers"));
+        list.add(new Department(3, "Eletronics"));
+        return list;
+    }
+}
